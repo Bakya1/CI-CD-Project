533fd375f0e95e9a4be08c8832a890d93c1f7534
Bakya1 <102711471+Bakya1@users.noreply.github.com>
2022-05-26 20:46:49 +0530

    Update main.tf

--- project documentation/main.tf
+++ project documentation/main.tf
@@ -20,8 +20,8 @@ resource "aws_security_group" "jenkins_sg" {
  
   ingress {
     description      = "Allow from Personal CIDR block"
-    from_port        = 8080
-    to_port          = 8080
+    from_port        = 
+    to_port          = 
     protocol         = "tcp"
     cidr_blocks      = [var.var1]
   }
